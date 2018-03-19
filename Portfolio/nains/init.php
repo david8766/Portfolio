6c60722a11dcafa351f538cfbada8129295028c5
Eleve O3W <webuser@objectif3w.web>
2018-03-19 11:39:00 +0100

    first commit

---
+++ Portfolio/nains/init.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * --------------------------------------------------
+ * BDD
+ * --------------------------------------------------
+ */
+
+define( 'DB_HOST', 'localhost' );
+define( 'DB_NAME', 'exotunnel' );
+define( 'DB_LOGIN', 'root' );
+define( 'DB_PWD', '' );
